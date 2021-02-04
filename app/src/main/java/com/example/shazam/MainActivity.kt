01e53b81931b8e333c6fae6d386b0fcd9c7a1d01
shegilo <ilorioluwasegun@gmail.com>
2021-02-04 20:08:10 +0100

    Shazam build

--- app/src/main/java/com/example/shazam/MainActivity.kt
+++ app/src/main/java/com/example/shazam/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.shazam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goNext(view: View) {
+        startActivity(Intent(this,MainActivityCheck::class.java))
     }
 }
---
+++ app/src/main/java/com/example/shazam/MainActivityCheck.kt
@@ -0,0 +1,11 @@
+package com.example.shazam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityCheck : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_check)
+    }
+}
